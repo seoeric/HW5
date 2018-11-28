99ba146d0e69fba1ebdc7fe5bf9bb09b1ec0928d
seoeric <seoeric@umich.edu>
2018-11-28 18:31:14 -0500

    Update Borrower

--- app/src/main/java/com/example/eric/hw5/BorrowerActivity.java
+++ app/src/main/java/com/example/eric/hw5/BorrowerActivity.java
@@ -6,13 +6,43 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class BorrowerActivity extends Activity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class BorrowerActivity extends Activity implements View.OnClickListener {
+    private EditText editTextBrBorrowedBy, editTextBrTitle, editTextBrAuthor, editTextBrCondition;
+    private Button buttonBrLookUp;
+    private FirebaseAuth mAuth;
+    private FirebaseDatabase database;
+    private DatabaseReference myRef;
+    private boolean isFound;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_borrower);
+
+        mAuth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference("Books");
+
+        editTextBrBorrowedBy = findViewById(R.id.editTextBrBorrowedBy);
+        editTextBrTitle = findViewById(R.id.editTextBrTitle);
+        editTextBrAuthor = findViewById(R.id.editTextBrAuthor);
+        editTextBrCondition = findViewById(R.id.editTextBrCondition);
+
+        buttonBrLookUp = findViewById(R.id.buttonBrLookUp);
+        buttonBrLookUp.setOnClickListener(this);
     }
 
     @Override
@@ -42,5 +72,37 @@ public class BorrowerActivity extends Activity {
                 return false;
 
         }
+    }
+
+    @Override
+    public void onClick(View v) {
+        isFound = false;
+        if (v == buttonBrLookUp) {
+            myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                        Book tempBookBr = snapshot.getValue(Book.class);
+                        if (tempBookBr.BorrowedBy == editTextBrBorrowedBy.getText().toString().toUpperCase()) {
+                            editTextBrTitle.setText(tempBookBr.Title);
+                            editTextBrAuthor.setText(tempBookBr.Author);
+                            editTextBrCondition.setText(tempBookBr.Condition);
+                            isFound = true;
+                        }
+                    }
+
+                    if (isFound = false) {
+                        Toast.makeText(BorrowerActivity.this, "Book not found", Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+
+                @Override
+                public void onCancelled(DatabaseError error) {
+                    // Failed to read value
+                }
+            });
+        }
+
     }
 }
--- app/src/main/java/com/example/eric/hw5/DeleteActivity.java
+++ app/src/main/java/com/example/eric/hw5/DeleteActivity.java
@@ -3,17 +3,51 @@ package com.example.eric.hw5;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class DeleteActivity extends Activity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class DeleteActivity extends Activity implements View.OnClickListener {
+    private EditText editTextDlTitle, editTextDlAuthor, editTextDlCondition, editTextDlBorrowedBy;
+    private Button buttonDlCheck, buttonDlDelete;
+    private FirebaseAuth mAuth;
+    private FirebaseDatabase database;
+    private DatabaseReference myRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_delete);
+        setContentView(R.layout.activity_home);
+
+        mAuth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference("Books");
+
+
+        editTextDlTitle = findViewById(R.id.editTextDlTitle);
+        editTextDlAuthor = findViewById(R.id.editTextDlAuthor);
+        editTextDlCondition = findViewById(R.id.editTextDlCondition);
+        editTextDlBorrowedBy = findViewById(R.id.editTextDlBorrowedBy);
+        buttonDlCheck = findViewById(R.id.buttonDlCheck);
+        buttonDlDelete = findViewById(R.id.buttonDlDelete);
+
+        buttonDlCheck.setOnClickListener(this);
+        buttonDlDelete.setOnClickListener(this);
     }
+
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater optionsMenuInflater = getMenuInflater();
@@ -41,5 +75,57 @@ public class DeleteActivity extends Activity {
                 return false;
 
         }
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == buttonDlCheck) {
+            if (editTextDlTitle.getText().toString() != "") {
+
+                myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        // This method is called once with the initial value and again
+                        // whenever data at this location is updated.
+                        if (dataSnapshot.child(mAuth.getUid()).child(editTextDlTitle.getText().toString().toUpperCase()).exists()) {
+                            Toast.makeText(DeleteActivity.this, "Book Found, updating values", Toast.LENGTH_SHORT).show();
+                            Book tempBook = dataSnapshot.child(mAuth.getUid()).child(editTextDlTitle.getText().toString().toUpperCase()).getValue(Book.class);
+                            editTextDlAuthor.setText(tempBook.Author);
+                            editTextDlBorrowedBy.setText(tempBook.BorrowedBy);
+                            editTextDlCondition.setText(tempBook.Condition);
+
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError error) {
+                        // Failed to read value
+                    }
+                });
+
+            }
+            else {
+                Toast.makeText(DeleteActivity.this, "Please Enter Book Title to Check", Toast.LENGTH_SHORT).show();
+            }
+        }
+        else if (v == buttonDlDelete) {
+            myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    // This method is called once with the initial value and again
+                    // whenever data at this location is updated.
+                    if (dataSnapshot.child(mAuth.getUid()).child(editTextDlTitle.getText().toString().toUpperCase()).exists()) {
+                        Toast.makeText(DeleteActivity.this, "Book Found, deleting book from database", Toast.LENGTH_SHORT).show();
+                        dataSnapshot.child(mAuth.getUid()).child(editTextDlTitle.getText().toString().toUpperCase()).getRef().removeValue();
+                    }
+                }
+
+                @Override
+                public void onCancelled(DatabaseError error) {
+                    // Failed to read value
+                }
+            });
+        }
+
     }
 }
